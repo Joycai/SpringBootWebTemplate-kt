e6793b2509d5020253fa80a5b0e28d305e7319bf
BigBaicai <joycai@joycai.cn>
2023-08-16 16:15:38 +0800

    update

--- src/main/kotlin/joycai/springboot/sample/mapper/UserMapper.kt
+++ src/main/kotlin/joycai/springboot/sample/mapper/UserMapper.kt
@@ -1,6 +1,7 @@
 package joycai.springboot.sample.mapper
 
 import joycai.springboot.sample.bean.User
+import joycai.springboot.sample.bean.UserDTO
 import joycai.springboot.sample.db.UserPO
 import org.mapstruct.Mapper
 import org.mapstruct.MappingConstants
@@ -9,5 +10,8 @@ import org.mapstruct.MappingConstants
 interface UserMapper {
 
     fun toPO(user: User): UserPO
-    fun toDTO(po: UserPO): User
+    fun toBO(po: UserPO): User
+    fun toDTO(user: User): UserDTO
+
+    fun fromDTO(userDTO: UserDTO): User
 }
